7a3877934523eb472f16a3b84b98ca613eb2340e
Rafael Dametto <cacotpds@gmail.com>
2023-08-04 13:13:45 -0300

    useLayaoutEffect

--- projectUseLayoutEffect/src/App.jsx
+++ projectUseLayoutEffect/src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react';
+import { useLayoutEffect, useRef, useState } from 'react';
 import './App.css';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [counted, setCounted] = useState([0, 1, 2, 3, 4]);
+
+  const divRef = useRef();
+
+  useLayoutEffect(() => {
+    const now = Date.now();
+    while (Date.now() < now + 600);
+    divRef.current.scrollTop = divRef.current.scrollHeight;
+  });
+
+  const handleClick = () => {
+    setCounted((c) => [...c, +c.slice(-1) + 1]);
+  };
 
   return (
     <>
@@ -16,18 +29,24 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>React - useLayoutEffect</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h2>Olá</h2>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <button onClick={handleClick}>Contador {counted.slice(-1)} </button>
+      </div>
+      <div
+        ref={divRef}
+        style={{ height: '200px', width: '100px', overflow: 'scroll' }}
+      >
+        {counted.map((c) => {
+          return <p key={`c-${c}`}> {c} </p>;
+        })}
+      </div>
     </>
   );
 }
